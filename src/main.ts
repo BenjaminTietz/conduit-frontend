1e05fe4037ee1c65394adc9c8096bb6912ecba21
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-12-09 10:39:13 +0100

    fix(bootstrap): load config before Angular bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
 import { AppModule } from "./app/app.module";
+import { ConfigService } from "./app/core/services/config.service";
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+const configService = new ConfigService();
+
+configService.loadConfig().then(() => {
+  platformBrowserDynamic([{ provide: ConfigService, useValue: configService }])
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+});
