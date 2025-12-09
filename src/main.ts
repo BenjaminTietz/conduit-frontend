ee55820612ad7b92421b48a61f18c17aeef17fae
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-12-09 12:39:20 +0100

    feat(config): load external environment config before bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
-import { enableProdMode, APP_INITIALIZER } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { ConfigService } from "./app/core/services/config.service";
 
-export function initConfig(config: ConfigService) {
-  return () => config.loadConfig();
+async function bootstrap() {
+  const configService = new ConfigService();
+  await configService.loadConfig();
+
+  platformBrowserDynamic([{ provide: ConfigService, useValue: configService }])
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
 }
 
-platformBrowserDynamic([
-  {
-    provide: APP_INITIALIZER,
-    useFactory: initConfig,
-    deps: [ConfigService],
-    multi: true,
-  },
-])
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrap();
