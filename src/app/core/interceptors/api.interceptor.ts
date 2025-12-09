e899e81579a45b9d209107ecf6da2ca34d869c48
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-12-09 10:39:23 +0100

    fix(api): use ConfigService apiUrl in HttpInterceptor

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,14 +6,21 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { ConfigService } from "../services/config.service";
 
-@Injectable({ providedIn: "root" })
+@Injectable()
 export class ApiInterceptor implements HttpInterceptor {
+  constructor(private config: ConfigService) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler,
   ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://localhost:8000/api${req.url}` });
+    const apiUrl = this.config.apiUrl;
+    const apiReq = req.clone({
+      url: `${apiUrl}${req.url}`,
+    });
+
     return next.handle(apiReq);
   }
 }
